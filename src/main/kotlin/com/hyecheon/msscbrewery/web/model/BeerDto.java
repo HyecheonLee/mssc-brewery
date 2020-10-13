e82351352d5feaa24481319986d846aaf67d95b4
hyecheon <rainbow880616@gmail.com>
2020-10-13 23:46:47 +0900

    Bean Validation

--- src/main/kotlin/com/hyecheon/msscbrewery/web/model/BeerDto.java
+++ src/main/kotlin/com/hyecheon/msscbrewery/web/model/BeerDto.java
@@ -6,8 +6,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -20,10 +23,29 @@ import java.util.UUID;
 public class BeerDto {
     @Null
     private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
+    private OffsetDateTime createdDate;
+
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
     @NotBlank
     private String beerName;
+
     @NotBlank
     private String beerStyle;
+
+    @NotNull
     @Positive
     private Long upc;
+
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
 }
